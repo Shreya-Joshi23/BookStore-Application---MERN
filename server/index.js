b22b27c9263e5da26d289af377f5ac1ba0dea6da
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2024-08-31 17:01:56 +0530

    cors changed

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const port=process.env.port || 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://book-store-application-mern.vercel.app/', 
+  origin: 'https://bookstore-application-mern-2.onrender.com/', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
